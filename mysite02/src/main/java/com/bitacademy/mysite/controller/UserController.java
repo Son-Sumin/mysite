a1c24f6a44cf9e7cae235a75381c9b2d14d5d94c
Son-Sumin <goldkitty007@gmail.com>
2022-11-09 10:54:52 +0900

    mysite02 -user#2

--- mysite02/src/main/java/com/bitacademy/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/bitacademy/mysite/controller/UserController.java
@@ -10,7 +10,17 @@ public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		request.setCharacterEncoding("utf-8");
+		
+		String action = request.getParameter("a");
+		
+		if("joinform".equals(action)) {
+			request
+				.getRequestDispatcher("/WEB-INF/views/user/joinform.jsp")
+				.forward(request, response);
+		} else if() {
+			
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {

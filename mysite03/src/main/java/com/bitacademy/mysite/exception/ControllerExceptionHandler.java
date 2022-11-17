3c11a58811db933e2d5d5310227a7de55adf2a99
Son-Sumin <goldkitty007@gmail.com>
2022-11-17 14:57:22 +0900

    mybatis #1

--- mysite03/src/main/java/com/bitacademy/mysite/exception/ControllerExceptionHandler.java
+++ mysite03/src/main/java/com/bitacademy/mysite/exception/ControllerExceptionHandler.java
@@ -15,13 +15,12 @@ public class ControllerExceptionHandler {
 	@ExceptionHandler(Exception.class)  // AOP의 when 지정
 	public String HandlerException(Model model, Exception e) {
 		// 로깅
-		StringWriter errors = new StringWriter();  // Stringbuffer 갖고 있는 StringWriter 보조스트림
+		StringWriter errors = new StringWriter();  // Stringbuffer 갖고 있는 StringWriter, 보조스트림
 		e.printStackTrace(new PrintWriter(errors));  // 주스트림  // 어떤 흐름, 어떤 메소드에서 발생했는지 알게끔
 		System.out.println(errors.toString());
 		
 		// 사과 페이지(HTML 응답, 정상종료)
-		model.addAttribute("exception", errors.toString());
-		
+		model.addAttribute("exception", errors.toString());	
 		return "error/exception";
 	}
 }
--- mysite03/src/main/java/com/bitacademy/mysite/exception/UserRepositoryException.java
+++ mysite03/src/main/java/com/bitacademy/mysite/exception/UserRepositoryException.java
@@ -11,3 +11,17 @@ public class UserRepositoryException extends RuntimeException {
 		super(message);
 	}
 }
+
+/*
+  reposiroty에서 발생한 에러(코드작성) Runtime때 발생시키기 위해
+  RuntimeException을 super로 둔 나의 exception 만들기(ex. UserRepositoryException.java)
+  
+  그리고 reposiroty 예외부분 수정
+  
+  spring-servlet.xml에서 base-package에 'com.bitacademy.mysite.exception' 추가
+  
+  프로그램 차원의 오류 - 500.jsp - web.xml 추가
+  con+ser+repo 차원의 오류 - exception.jsp
+  
+ 
+ */

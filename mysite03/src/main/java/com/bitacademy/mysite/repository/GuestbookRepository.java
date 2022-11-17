a0f9aaf66b38b349f21dfb008e9011d4feca6964
Son-Sumin <goldkitty007@gmail.com>
2022-11-17 19:16:13 +0900

    guestbook03

--- mysite03/src/main/java/com/bitacademy/mysite/repository/GuestbookRepository.java
+++ mysite03/src/main/java/com/bitacademy/mysite/repository/GuestbookRepository.java
@@ -1,12 +1,8 @@
 package com.bitacademy.mysite.repository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,141 +16,20 @@ public class GuestbookRepository {
 	private SqlSession sqlSession;
 	
 	public Boolean insert(GuestbookVo vo) {
-		boolean result = false;
-		
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		
-		try {
-			conn = getConnection();
-			
-			String sql = " insert into guestbook values(null, ?, ?, ?, now())";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, vo.getName());
-			pstmt.setString(2, vo.getPassword());
-			pstmt.setString(3, vo.getContents());
-			
-			int count = pstmt.executeUpdate();
-			
-			result = count == 1;
-			
-		} catch (SQLException e) {
-			System.out.println("Error:" + e);
-		} finally {
-			try {
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				if(conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return result;
+		int count = sqlSession.insert("guestbook.insert", vo);
+		return count ==1;
 	}
 	
 	
 	public List<GuestbookVo> findAll() {
 		return sqlSession.selectList("guestbook.findAll");
-
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-
-		try {
-			conn = getConnection();
-
-			String sql = 
-					" select no, name, contents, date_format(reg_date, '%Y/%m/%d %H:%i:%s')" + 
-					" from guestbook order by reg_date desc";
-			pstmt = conn.prepareStatement(sql);
-			
-			rs = pstmt.executeQuery();
-			while (rs.next()) {
-				Long no = rs.getLong(1);
-				String name = rs.getString(2);
-				String contents = rs.getString(3);
-				String regDate = rs.getString(4);
-
-				GuestbookVo vo = new GuestbookVo();
-				vo.setNo(no);
-				vo.setName(name);
-				vo.setContents(contents);
-				vo.setRegDate(regDate);
-
-				result.add(vo);
-			}
-
-		} catch (SQLException e) {
-			System.out.println("Error: " + e);
-		} finally {
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null)
-					conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return result;
 	}
 	
 	
 	public Boolean deleteByNoAndPassword(Long no, String password) {
-		boolean result = false;
-		
-		Connection conn = null;  
-		PreparedStatement pstmt = null;
-		
-		try {
-			conn = getConnection();
-			
-			String sql = " delete from guestbook where no = ? and password = ?";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setLong(1, no);
-			pstmt.setString(2, password);
-			
-			int count = pstmt.executeUpdate();
-
-			result = count == 1;
-			
-		} catch (SQLException e) {
-			System.out.println("Error: " + e);
-		} finally {
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null)
-					conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return result;
+		Map<String, Object> map = new HashMap<>();
+		map.put("no", no);
+		map.put("password", password);
+		return sqlSession.selectOne("guestbook.deleteByNoAndPassword", map);
 	}
-	
-	
-	private Connection getConnection() throws SQLException {
-		Connection conn = null;
-
-		try {
-			Class.forName("org.mariadb.jdbc.Driver");
-
-			String url = "jdbc:mysql://127.0.0.1:3306/webdb?charset=utf8";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
-			
-		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 로딩 실패: " + e);
-		}
-		return conn;
-	}
-	
 }

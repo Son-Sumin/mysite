ef9a6e308b1fb4342cbff5b543afd002726880cc
Son-Sumin <goldkitty007@gmail.com>
2022-11-17 16:35:17 +0900

    mybatis #5

--- mysite03/src/main/java/com/bitacademy/mysite/repository/GuestbookRepository.java
+++ mysite03/src/main/java/com/bitacademy/mysite/repository/GuestbookRepository.java
@@ -8,12 +8,16 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.bitacademy.mysite.vo.GuestbookVo;
 
 @Repository
 public class GuestbookRepository {
+	@Autowired
+	private SqlSession sqlSession;
 	
 	public Boolean insert(GuestbookVo vo) {
 		boolean result = false;
@@ -53,7 +57,7 @@ public class GuestbookRepository {
 	
 	
 	public List<GuestbookVo> findAll() {
-		List<GuestbookVo> result = new ArrayList<>();
+		return sqlSession.selectList("guestbook.findAll");
 
 		Connection conn = null;
 		PreparedStatement pstmt = null;

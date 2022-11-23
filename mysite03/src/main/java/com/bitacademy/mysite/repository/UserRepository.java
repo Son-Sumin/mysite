c28978d6a16fcc60843f7dd438d6127dbf5a7887
Son-Sumin <goldkitty007@gmail.com>
2022-11-23 13:54:01 +0900

    stop watch

--- mysite03/src/main/java/com/bitacademy/mysite/repository/UserRepository.java
+++ mysite03/src/main/java/com/bitacademy/mysite/repository/UserRepository.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StopWatch;
 
 import com.bitacademy.mysite.vo.UserVo;
 
@@ -24,10 +25,20 @@ public class UserRepository {
 	}
 	
 	public UserVo findByEmailAndPassword(String email, String password) {
+		// 어플의 실용성을 위해 실행 시간 재기 -> 코드 수정 발생 -> AOP @Around 사용하기
+		StopWatch sw = new StopWatch();
+		sw.start();
+		
 		Map<String, Object> map = new HashMap<>();
 		map.put("email", email);
 		map.put("password", password);
-		return sqlSession.selectOne("user.findByEmailAndPassword", map);
+		UserVo result = sqlSession.selectOne("user.findByEmailAndPassword", map);
+		
+		sw.stop();
+		Long totalTime = sw.getTotalTimeMillis();
+		System.out.println("---------> " + totalTime);
+		
+		return result;
 	}
 	
 	public Boolean insert(UserVo vo) {

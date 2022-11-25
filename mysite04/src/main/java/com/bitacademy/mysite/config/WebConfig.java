b0ff3298ce95418c6e1a4b8b8cc40a47faa567e6
Son-Sumin <goldkitty007@gmail.com>
2022-11-25 11:49:39 +0900

    Java Config - Web #1

--- mysite04/src/main/java/com/bitacademy/mysite/config/WebConfig.java
+++ mysite04/src/main/java/com/bitacademy/mysite/config/WebConfig.java
@@ -5,10 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 
+import com.bitacademy.mysite.config.web.FileuploadConfig;
+import com.bitacademy.mysite.config.web.MVCConfig;
+import com.bitacademy.mysite.config.web.SecurityConfig;
+
 @Configuration
 @EnableAspectJAutoProxy
 @ComponentScan({"com.bitacademy.mysite.controller" , "com.bitacademy.mysite.exception"})
-@Import({})
+@Import({MVCConfig.class, SecurityConfig.class, FileuploadConfig.class})
 public class WebConfig {
 
 }

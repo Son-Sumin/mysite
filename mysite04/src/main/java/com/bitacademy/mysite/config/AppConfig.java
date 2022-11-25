93b8e8dabc4e6403a8710ae3509e00364b2f9351
Son-Sumin <goldkitty007@gmail.com>
2022-11-25 10:48:03 +0900

    App Java Config - properties

--- mysite04/src/main/java/com/bitacademy/mysite/config/AppConfig.java
+++ mysite04/src/main/java/com/bitacademy/mysite/config/AppConfig.java
@@ -11,6 +11,8 @@ import com.bitacademy.mysite.config.app.MyBatisConfig;
 @Configuration
 @EnableAspectJAutoProxy
 @ComponentScan({"com.bitacademy.mysite.service", "com.bitacademy.mysite.repository", "com.bitacademy.mysite.aspect"})
-@Import({DBConfig.class, MyBatisConfig.class})  // 한번에 쓰지 말고 설정들을 클래스로 분리하여 관리
+@Import({DBConfig.class, MyBatisConfig.class})
 public class AppConfig {
 }
+
+// 한번에 쓰지 말고 설정들을 클래스로 분리하여 관리

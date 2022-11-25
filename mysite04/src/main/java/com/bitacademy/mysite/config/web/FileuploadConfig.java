601a95624f9050a3c0bcf721c9027bf3818607f8
Son-Sumin <goldkitty007@gmail.com>
2022-11-25 11:53:51 +0900

    Java Config - Web #1

--- mysite04/src/main/java/com/bitacademy/mysite/config/web/FileuploadConfig.java
+++ mysite04/src/main/java/com/bitacademy/mysite/config/web/FileuploadConfig.java
@@ -32,8 +32,6 @@ public class FileuploadConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry
 			.addResourceHandler(env.getProperty("fileupload.resourceMapping"))
-			.addResourceLocations(env.getProperty("fileupload.uploadLocation"));
+			.addResourceLocations("file:" + env.getProperty("fileupload.uploadLocation"));
 	}
-	
-	
 }

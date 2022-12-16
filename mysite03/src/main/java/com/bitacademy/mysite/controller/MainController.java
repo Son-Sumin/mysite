0143f4f1526ed1092eeddb7ccd3bd82dfaa9b671
Son-Sumin <goldkitty007@gmail.com>
2022-12-16 10:06:27 +0900

    message converter

--- mysite03/src/main/java/com/bitacademy/mysite/controller/MainController.java
+++ mysite03/src/main/java/com/bitacademy/mysite/controller/MainController.java
@@ -1,9 +1,15 @@
 package com.bitacademy.mysite.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.bitacademy.mysite.vo.UserVo;
 
 @Controller
 public class MainController {
@@ -13,5 +19,44 @@ public class MainController {
 	public String index() {
 		Logger.debug("MainController.index() called");
 		return "main/index";  // WEB-INF/web.xml 참고
+	}
+	
+	// Message Converter 실습
+	// String; 한글 넣으면 깨짐 -> utf-8 encoding setting 필요
+	@ResponseBody
+	@RequestMapping("/msg01")
+	public String message01() {
+		return "안녕";
+	}
+	
+	// Object
+	@ResponseBody
+	@RequestMapping("/msg02")
+	public Object message02() {
+		UserVo vo = new UserVo();
+		vo.setNo(1L);
+		vo.setEmail("kickscar@gmail.com");
+		vo.setName("맹구");
+		return vo;
+	}
+	
+	@ResponseBody
+	@RequestMapping("/msg03")
+	public Object message03() {
+		List<UserVo> list = new ArrayList<>();
+		
+		UserVo vo1 = new UserVo();
+		vo1.setNo(1L);
+		vo1.setEmail("kickscar1@gmail.com");
+		vo1.setName("맹구1");
+		list.add(vo1);
+		
+		UserVo vo2 = new UserVo();
+		vo2.setNo(2L);
+		vo2.setEmail("kickscar2@gmail.com");
+		vo2.setName("맹구2");
+		list.add(vo2);
+		
+		return list;
 	}
 }
--- mysite05/src/main/java/com/bitacademy/mysite/config/web/MVCConfig.java
+++ mysite05/src/main/java/com/bitacademy/mysite/config/web/MVCConfig.java
@@ -1,7 +1,17 @@
 package com.bitacademy.mysite.config.web;
 
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -24,6 +34,43 @@ public class MVCConfig implements WebMvcConfigurer {
 		return viewResolver;
 	}
 	
+	/* Message Converters */
+	// mysite03 msg01
+	@Bean
+	public StringHttpMessageConverter stringHttpMessageConverter() {
+		StringHttpMessageConverter messageConverter = new StringHttpMessageConverter();
+		messageConverter.setSupportedMediaTypes(
+				Arrays.asList(
+					new MediaType("text", "html", Charset.forName("utf-8"))
+				)
+		);
+		
+		return messageConverter;
+	}
+	
+	@Bean
+	public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+		Jackson2ObjectMapperBuilder builder =
+				new Jackson2ObjectMapperBuilder()
+					.indentOutput(true)
+					.dateFormat(new SimpleDateFormat("YYYY-mm-dd"));
+		
+		MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter(builder.build());
+		messageConverter.setSupportedMediaTypes(
+				Arrays.asList(
+					new MediaType("application", "json", Charset.forName("utf-8"))
+				)
+		);
+		
+		return messageConverter;
+	}
+	
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(stringHttpMessageConverter());
+		converters.add(mappingJackson2HttpMessageConverter());
+	}
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry
@@ -32,7 +79,7 @@ public class MVCConfig implements WebMvcConfigurer {
 	}
 
 	/* 서블릿 컨테이너의 디폴트 서블릿 위임 핸들러 */
-	// @Override
+	// @Override - web.xml 없애기 위해 없앰
 	// public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 	// 	configurer.enable();
 	// }
